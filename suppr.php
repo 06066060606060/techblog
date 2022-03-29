3db6f478e075f48f3e6e8dea03609a7e10fe8578
06066060606060 <xmicky+git@hotmail.fr>
2022-03-29 14:39:51 +0400

    commit cours

--- suppr.php
+++ suppr.php
@@ -5,6 +5,6 @@ if (isset($_GET['id']) and !empty($_GET['id'])) {
 
     $suppr = $bdd->prepare('DELETE FROM articles WHERE id = ?');
     $suppr->execute(array($suppr_id));
-   header('location: index.php');
+   header('location: test-index.php');
 }
  ?>
